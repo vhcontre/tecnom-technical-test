0de6766150ac71dec149bb93b3bca2db187bae25
Victor Contreras <vcontreras@gmail.com>
2025-07-17 22:05:25 -0300

    feat: add ProductController, Swagger, and service/repo DI setup
    - Se agregó el controlador ´ProductController´ con endpoint GET para productos
    - Se integró AutoMapper en DI con perfil personalizado
    - Se configuró Swagger con información de contacto y documentación XML
    - Se agregó ´AppDbContext´ con conexión SQL Server desde configuración
    - Se registraron ´ProductRepository´ e ´ProductService´ en el contenedor DI
    
    tecnom-03

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+/// <summary>
+/// 
+/// </summary>
+[Route("api/[controller]")]
+[ApiController]
+public class ProductController : ControllerBase
+{
+    private readonly ProductService _productService;
+    private readonly IMapper _mapper;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="productService"></param>
+    /// <param name="mapper"></param>
+    public ProductController(ProductService productService, IMapper mapper)
+    {
+        _productService = productService;
+        _mapper = mapper;
+    }
+
+    /// <summary>
+    /// Obtener todos los productos.
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ProductDTO>>> GetAll()
+    {
+        var products = await _productService.GetAllProductsAsync();
+        return Ok(_mapper.Map<IEnumerable<ProductDTO>>(products));
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
 using FluentValidation;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using technical_tests_backend_ssr.Data;
+using technical_tests_backend_ssr.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.AddControllers();
 builder.Services.AddValidatorsFromAssemblyContaining<ProductDTOValidator>();
@@ -15,7 +21,50 @@ builder.Services.AddCors(options =>
 
 builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Challenge Técnico: Back-End Dev (NET) - API Technical tests",
+        Version = "v1",
+        Description = "API para gestionar servicios",
+        Contact = new OpenApiContact
+        {
+            Name = "Víctor Hugo Contreras",
+            Email = "vcontreras@gmail.com"
+        }
+    });
+
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ProductService>();
+
+var mapperConfig = new MapperConfiguration(cfg => { cfg.AddProfile(new AutoMapperProfile()); });
+IMapper mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
 var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Technical API v1");
+    });
+}
 
 app.UseCors("AllowAll");
 app.UseAuthorization();
